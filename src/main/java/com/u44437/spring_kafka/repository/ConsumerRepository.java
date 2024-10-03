58808ef1c07c8790986683ee7f190085d4665170
44437 <ercantopuz33@gmail.com>
2024-10-03 22:21:11 +0300

    Print the key values consumed

--- src/main/java/com/u44437/spring_kafka/repository/ConsumerRepository.java
+++ src/main/java/com/u44437/spring_kafka/repository/ConsumerRepository.java
@@ -1,10 +1,14 @@
 package com.u44437.spring_kafka.repository;
 
+import com.u44437.spring_kafka.util.Constants;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ConsumerRepository {
-  @KafkaListener(topics = "first")
-  public void consumeMessage(String message) {
-    System.out.println(message);
+  @KafkaListener(topics = Constants.TOPIC_FIRST)
+  public void consumeMessage(ConsumerRecord<String, String> record) {
+    System.out.printf("%d-%s \n", record.offset(), record.value());
   }
 }
