f10ba3d3f1968fa63127814766103ab97fc5ce4e
44437 <ercantopuz33@gmail.com>
2024-10-07 17:24:09 +0300

    Enable timeout and retry mechanisms

--- src/main/java/com/u44437/spring_kafka/conf/FrontOfficeConfiguration.java
+++ src/main/java/com/u44437/spring_kafka/conf/FrontOfficeConfiguration.java
@@ -29,7 +29,10 @@ public class FrontOfficeConfiguration {
     return Map.of(
       ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094",
       ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
-      ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+      ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class,
+      ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, 5000,// this >= linger.ms(default=0) + request.timout.ms(default=30sec)
+      ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 3000,// 3 is adequate
+      ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);// infinite
 //      ProducerConfig.PARTITIONER_CLASS_CONFIG, RoundRobinPartitioner.class); // the default one is viable for now
   }
 
