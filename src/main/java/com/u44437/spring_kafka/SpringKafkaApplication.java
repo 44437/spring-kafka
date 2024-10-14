5bd8c499e1dae06d7ff8517979a9757f2ecd261f
44437 <ercantopuz33@gmail.com>
2024-10-14 20:41:06 +0300

    Remove the unnecessary annotation

--- src/main/java/com/u44437/spring_kafka/SpringKafkaApplication.java
+++ src/main/java/com/u44437/spring_kafka/SpringKafkaApplication.java
@@ -2,9 +2,7 @@ package com.u44437.spring_kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
-@EnableKafka
 @SpringBootApplication
 public class SpringKafkaApplication {
 
