85946e9118eab8ee6091a3a457a07bd20014161f
44437 <ercantopuz33@gmail.com>
2024-10-14 16:39:33 +0300

    v2 - kafka streams w/ streams dsl, custom configuration and w/o @EnableKafkaStreams

--- src/main/java/com/u44437/spring_kafka/SpringKafkaApplication.java
+++ src/main/java/com/u44437/spring_kafka/SpringKafkaApplication.java
@@ -3,11 +3,9 @@ package com.u44437.spring_kafka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @EnableKafka
 @SpringBootApplication
-@EnableKafkaStreams
 public class SpringKafkaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/u44437/spring_kafka/conf/StreamsConfiguration.java
+++ src/main/java/com/u44437/spring_kafka/conf/StreamsConfiguration.java
@@ -1,35 +1,53 @@
 package com.u44437.spring_kafka.conf;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
-import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.springframework.context.annotation.DependsOn;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Properties;
 
 @Configuration
 public class StreamsConfiguration {
 
-  @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
-  public KafkaStreamsConfiguration kStreamsConfig() {
-    Map<String, Object> props = new HashMap<>();
+  @Bean
+  public Properties properties() {
+    Properties props = new Properties();
     props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streaming-app");
     props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
     props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
     props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-    return new KafkaStreamsConfiguration(props);
+    return props;
   }
 
   @Bean
-  public KStream<String, String> kStream(StreamsBuilder streamsBuilder) {
+  @DependsOn("kStream")
+  public KafkaStreams kafkaStreams(StreamsBuilder streamsBuilder, Properties properties) {
+    KafkaStreams kafkaStreams = new KafkaStreams(streamsBuilder.build(), properties);
+
+    Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+
+    kafkaStreams.start();
+
+    return kafkaStreams;
+  }
+
+  @Bean
+  public KStream kStream (StreamsBuilder streamsBuilder) {
     KStream<String, String> stream = streamsBuilder.stream("input-topic");
     stream.foreach((key, value) -> System.out.println("Key: " + key + ", Value: " + value));
+
     stream.to("output-topic");
+
     return stream;
+  }
+
+  @Bean
+  public StreamsBuilder streamsBuilder() {
+    return new StreamsBuilder();
   }
 }
